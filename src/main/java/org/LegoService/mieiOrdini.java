b45a916abd228b3ba42b7d9276bc82a3e94b957d
andrenada <150192703+andrenada@users.noreply.github.com>
2024-07-09 21:09:54 +0200

    Add files via upload

---
+++ src/main/java/org/LegoService/mieiOrdini.java
@@ -0,0 +1,15 @@
+package org.LegoService;
+
+import javax.persistence.Table;
+import java.sql.Date;
+import java.sql.Types;
+import java.util.List;
+
+
+@Table(name = "mieiOridni")
+public class mieiOrdini {
+    public int NumeroOrdine;
+    public String Stato;
+    public Date DataOrdine;
+    public Date DataSpedizione;
+}
